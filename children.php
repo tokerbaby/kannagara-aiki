f3a607cff768b4f9b7a1e173b48e18dfffdd7528
tokerbaby <daryatoker@gmail.com>
2018-02-10 23:02:02 +0300

    Refactor all pages to php extension. Try 2

--- children.php
+++ children.php
@@ -10,119 +10,27 @@
         <meta name="description" content="The HTML5 Herald">
         <meta name="author" content="SitePoint">
 
-        <link rel="stylesheet" href="css/index.css">
+        <link rel="stylesheet" type="text/css" href="css/children.css">
+        <link rel="stylesheet" type="text/css" href="css/header.css">
+        <link rel="stylesheet" type="text/css" href="css/footer.css">
+        <link rel="stylesheet" type="text/css" href="css/general.css">
 
         <!-- Bootstrap CSS -->
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
     </head>
 
     <body>
-        <div class="container" style="background-color:lightblue;">
-            <div class="container header_container" style="background-color:pink;">
-
-                <div class="row">
-                    <div class="col-3">
-                        <div class="text-left" style="background-color:aliceblue;">
-                            <img src="images/logo.gif" alt="Логотип Каннагара Додзё">
-                        </div>
-                    </div>
-
-                    <div class="col-9 text-right">
-                        <nav class="navbar navbar-toggleable-md navbar-light d-inline-block">
-                            <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                                <span class="navbar-toggler-icon"></span>
-                            </button>
-
-                            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                                <ul class="navbar-nav mr-auto">
-                                    <li class="nav-item">
-                                        <a class="nav-link menu_link active" href="#">Беседы о Синто<span class="sr-only">(current)</span></a>
-                                    </li>
-
-                                    <li class="nav-item">
-                                        <div class="dropdown">
-                                            <button class="dropbtn menu_dropdown">О клубе
-                                            </button>
-                                            <div class="dropdown-content">
-                                                <a href="#">Преподаватели</a>
-                                                <a href="#">Наставники и коллеги</a>
-                                                <a href="#">Экзамены</a>
-                                                <a href="#">Правила</a>
-                                            </div>
-                                        </div>
-                                    </li>
-
-                                    <li class="nav-item">
-                                        <div class="dropdown">
-                                            <button class="dropbtn menu_dropdown">Детский мир айкидо
-                                            </button>
-                                            <div class="dropdown-content">
-                                                <a href="#">Айкидо для школьников</a>
-                                                <a href="#">Айкидо для дошкольников</a>
-                                                <a href="#">Перед первым занятием</a>
-                                            </div>
-                                        </div>
-                                    </li>
-
-                                    <li class="nav-item">
-                                        <div class="dropdown">
-                                            <button class="dropbtn menu_dropdown">О занятиях
-                                            </button>
-                                            <div class="dropdown-content">
-                                                <a href="#">Расписание</a>
-                                                <a href="#">Стоимость</a>
-                                                <a href="#">Новичку</a>
-                                                <a href="#">Где купить</a>
-                                            </div>
-                                        </div>
-                                    </li>
-
-                                    <li class="nav-item">
-                                        <a class="nav-link menu_link" href="#">Контакты<span class="sr-only">(current)</span></a>
-                                    </li>
-
-                                    <li class="nav-item">
-                                        <div class="dropdown">
-                                            <button class="dropbtn menu_dropdown">Семинары</button>
-                                            <div class="dropdown-content">
-                                                <a href="#">О семинарах</a>
-                                                <a href="#">Международный семинар на Лазурном берегу в июне 2018</a>
-                                                <a href="#">Семинар в Марокко, апрель 2018</a>
-                                                <a href="#">Семинар Поля Мюллера-сэнсэя, ноябрь 2017</a>
-                                                <a href="#">Семинар по айкидо на Лазурном берегу 8-15 октября 2017</a>
-                                                <a href="#">Архив семинаров</a>
-                                                <a href="#">Фото</a>
-                                            </div>
-                                        </div>
-                                    </li>
-
-                                    <li class="nav-item">
-                                        <a class="nav-link menu_link active" href="#">Видео<span class="sr-only">(current)</span></a>
-                                    </li>
-
-                                    <li class="nav-item">
-                                        <a class="nav-link menu_link" href="#">En<span class="sr-only">(current)</span></a>
-                                    </li>
-
-                                    <li class="nav-item">
-                                        <a class="nav-link menu_link" href="#">Вход<span class="sr-only">(current)</span></a>
-                                    </li>
-
-                                </ul>
-                            </div>
-                        </nav>
-                    </div>
-                </div>
-            </div>
+        <div class="container-fluid header_container">
+            <?php include('header.php') ?>
         </div>
 
-        <div class="container">
+        <div class="container text-center">
             <h1>
                 Айкидо для школьников
                 <br>
                 <small>Приглашаем ребят в детскую школу айкидо при Московском Айкидо Каннагара Додзё</small>
             </h1>
-            <img src="images/boy.jpg" id="boy">
+            <img src="images/children/boy.jpg" id="boy">
             <p>Мы не отбираем «способных», не стремимся вырастить чемпионов или мускулистых победителей, не знающих сомнения в правоте силы. Хорошо освоенное боевое искусство - это острое оружие, и человек, владеющий им, должен, прежде всего, уметь владеть собой.</p>
             <p>За четверть века преподавания айкидо мы накопили определённый опыт и хотим передать часть наших знаний молодому поколению. Мы хотим передать знания боевых искусств, знание серьезных навыков самообороны, вместе с уважением к живому и чувством ответственности за применение силы.</p>
             <p>Занятия айкидо в наших группах помогут ребятам окрепнуть физически, улучшат координацию, помогут достойно выйти из схватки с обидчиками, а также позволят приобрести новых друзей!</p>
--- index.php
+++ index.php
@@ -11,6 +11,10 @@
 		<meta name="author" content="SitePoint">
 
 	  	<link rel="stylesheet" href="css/index.css">
+	  	<link rel="stylesheet" type="text/css" href="css/general.css">
+	  	<link rel="stylesheet" type="text/css" href="css/header.css">
+	  	<link rel="stylesheet" type="text/css" href="css/footer.css">
+	  	<link rel="stylesheet" type="text/css" href="css/mainpage.css">
 
 	  	<!-- Bootstrap CSS -->
 	  	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
@@ -19,110 +23,16 @@
 	<body>
 		
   		<div class="container-fluid header_container">
-  			<div class="container">
-				<div class="row">
-					<div class="col-3">
-		  				<div class="text-left">
-		  					<img src="images/logo.gif" alt="Логотип Каннагара Додзё">
-					    </div>
-					</div>
-			
-					<div class="col-9 text-right">
-						<nav class="navbar navbar-toggleable-md navbar-light d-inline-block">
-						  	<button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-dty="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-						    	<span class="navbar-toggler-icon"></span>
-						  	</button>
 
-						  	<div class="collapse navbar-collapse" id="navbarSupportedContent">
-							    <ul class="navbar-nav mr-auto">
-							    	<li class="nav-item">
-							        	<a class="nav-link menu_link active" href="#">Lol<span class="sr-only">(current)</span></a>
-							        </li>
-
-							        <li class="nav-item">
-							        	<div class="dropdown">
-									  		<button class="dropbtn menu_dropdown">О клубе
-									  		</button>
-									 		<div class="dropdown-content">
-								    			<a href="#">Преподаватели</a>
-								    			<a href="#">Наставники и коллеги</a>
-								    			<a href="#">Экзамены</a>
-								    			<a href="#">Правила</a>
-									  		</div>
-										</div>
-							        </li>
-
-							    	<li class="nav-item">
-							        	<div class="dropdown">
-									  		<button class="dropbtn menu_dropdown">Детский мир айкидо
-									  		</button>
-									 		<div class="dropdown-content">
-								    			<a href="#">Айкидо для школьников</a>
-								    			<a href="#">Айкидо для дошкольников</a>
-								    			<a href="#">Перед первым занятием</a>
-									  		</div>
-										</div>
-							        </li>
-
-							        <li class="nav-item">
-							        	<div class="dropdown">
-									  		<button class="dropbtn menu_dropdown">О занятиях
-									  		</button>
-									 		<div class="dropdown-content">
-								    			<a href="#">Расписание</a>
-								    			<a href="#">Стоимость</a>
-								    			<a href="#">Новичку</a>
-								    			<a href="#">Где купить</a>
-									  		</div>
-										</div>
-							        </li>
-
-							        <li class="nav-item">
-							        	<a class="nav-link menu_link" href="#">Контакты<span class="sr-only">(current)</span></a>
-							        </li>
-
-							        <li class="nav-item">
-								        <div class="dropdown">
-										  	<button class="dropbtn menu_dropdown">Семинары</button>
-										  	<div class="dropdown-content">
-											    <a href="#">О семинарах</a>
-											    <a href="#">Международный семинар на Лазурном берегу в июне 2018</a>
-											    <a href="#">Семинар в Марокко, апрель 2018</a>
-											    <a href="#">Семинар Поля Мюллера-сэнсэя, ноябрь 2017</a>
-											    <a href="#">Семинар по айкидо на Лазурном берегу 8-15 октября 2017</a>
-											    <a href="#">Архив семинаров</a>
-											    <a href="#">Фото</a>
-										  	</div>
-										</div>
-							      	</li>
-
-							      	<li class="nav-item">
-							        	<a class="nav-link menu_link active" href="#">Видео<span class="sr-only">(current)</span></a>
-							        </li>
-
-							      	<li class="nav-item">
-							        	<a class="nav-link menu_link" href="#">En<span class="sr-only">(current)</span></a>
-							        </li>
-
-							        <li class="nav-item">
-							        	<a class="nav-link menu_link" href="#">Вход<span class="sr-only">(current)</span></a>
-							        </li>
-
-							    </ul>
-						  	</div>
-						</nav>
-					</div>
-				</div>
-			</div>
+			<?php include('header.php') ?>
 
 			<div class="container">
 				<div class="about">
 					<h1>
-						О клубе
-						<br>
-						<small>Айкидо - боевое искусство в поисках мира</small>
+					Айкидо - боевое искусство в поисках мира
+					<br>
+					<small>Айкидо для нас не бизнес. Для нас это Путь. Мы ищем единомышленников.</small>
 					</h1>
-					<p><strong>Айкидо для нас не бизнес. Для нас это Путь. Мы ищем единомышленников.</strong></p>
 					<p>Наше додзё основано в 1989 г. С тех пор занятия идут, не прекращаясь. Мы видим айкидо не только как острое оружие, но и как возможность развития личности.</p>
 				</div>
 			</div>
