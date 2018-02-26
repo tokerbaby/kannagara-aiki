f343cefc53ed737ec18ad9f7f919d37fe544450e
tokerbaby <daryatoker@gmail.com>
2018-02-26 17:59:27 +0300

    Added modal login form to the homepage

---
+++ signup.php
@@ -0,0 +1,37 @@
+<div class="col-lg-6 col-12 pb-3">
+                    <div class="card h-100">
+                        <div class="card-body pt-3">
+                            <h2 class="text-center mb-4">Регистрация</h2>
+                            <ul class="list-inline text-center py-2">
+                                <li class="list-inline-item px-3"><a href="" title="Twitter"><i class="display-3 ion-social-twitter-outline"></i></a>&nbsp; </li>
+                                <li class="list-inline-item px-3"><a href="" title="Google"><i class="display-3 ion-social-googleplus-outline"></i></a>&nbsp; </li>
+                                <li class="list-inline-item px-3"><a href="" title="Facebook"><i class="display-3 ion-social-facebook-outline"></i></a></li>
+                            </ul>
+                            <form role="form">
+                                <div class="form-group">
+                                    <label for="input2EmailForm" class="sr-only form-control-label">Электропочта</label>
+                                    <div class="mx-auto col-sm-10">
+                                        <input type="text" class="form-control" id="input2EmailForm" placeholder="Электронная почта" required="">
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    <label for="input2PasswordForm" class="sr-only form-control-label">Пароль</label>
+                                    <div class="mx-auto col-sm-10">
+                                        <input type="text" class="form-control" id="input2PasswordForm" placeholder="Пароль" required="">
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    <label for="input2Password2Form" class="sr-only form-control-label">Подтвердить пароль</label>
+                                    <div class="mx-auto col-sm-10">
+                                        <input type="text" class="form-control" id="input2Password2Form" placeholder="Подтвердить пароль" required="">
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    <div class="mx-auto col-sm-10 pb-3 pt-2">
+                                        <button type="submit" class="btn btn-outline-secondary btn-lg btn-block">Зарегистрироваться</button>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
