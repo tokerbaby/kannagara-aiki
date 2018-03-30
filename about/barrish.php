26f3477f3598e592da3e3159936c4505e4d796a0
tokerbaby <daryatoker@gmail.com>
2018-03-30 23:07:21 +0300

    Add all mentors' personal pages and also two seminars - inter-club and Marocco seminar

---
+++ about/barrish.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: gwyn0431
+ * Date: 3/30/18
+ * Time: 5:13 PM
+ */
---
+++ about/nishio.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: gwyn0431
+ * Date: 3/30/18
+ * Time: 7:15 PM
+ */
